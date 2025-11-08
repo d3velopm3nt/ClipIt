00f1a85f4e7f30aaaa0d03ce7bf46a95e89c403a
JayJay <d3velopm3nt@gmail.com>
2025-11-08 07:58:14 +0200

    Refactor library path retrieval in copy_past_ffi.dart to dynamically determine the location of the Windows paste library based on the build mode. Update type definitions for clarity and improve code organization.

--- lib/hotkey/copy_past_ffi.dart
+++ lib/hotkey/copy_past_ffi.dart
@@ -1,17 +1,38 @@
 import 'dart:ffi' as ffi;
-import 'dart:io' show Directory;
+import 'dart:io' show Directory, File;
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as path;
 
-typedef copy_paste_func = ffi.Void Function();
+typedef CopyPasteFunc = ffi.Void Function();
 
 typedef CopyPaste = void Function();
-var releasePath = path.join(Directory.current.path,"data\\flutter_assets");
-var debugPath = Directory.current.path;
-var libraryPath =
-    path.join(kReleaseMode ? releasePath : debugPath, "assets\\libs" ,'WindowsPasteLibrary.dll');
+// Get the path to the Windows paste library
+String getLibraryPath() {
+  if (kReleaseMode) {
+    // In release mode, the DLL should be in the same directory as the executable
+    return 'WindowsPasteLibrary.dll';
+  } else {
+    // In debug mode, try multiple possible locations
+    final possiblePaths = [
+      'assets/libs/WindowsPasteLibrary.dll',  // From project root when running flutter run
+      'data/flutter_assets/assets/libs/WindowsPasteLibrary.dll',  // Flutter's asset path
+      path.join(Directory.current.path, 'assets', 'libs', 'WindowsPasteLibrary.dll'),
+    ];
+
+    for (final possiblePath in possiblePaths) {
+      if (File(possiblePath).existsSync()) {
+        return possiblePath;
+      }
+    }
+
+    // Fallback to the most likely debug path
+    return path.join(Directory.current.path, 'assets', 'libs', 'WindowsPasteLibrary.dll');
+  }
+}
+
+var libraryPath = getLibraryPath();
 
 final dll = ffi.DynamicLibrary.open(libraryPath);
 
 final CopyPaste pasteToWindows =
-    dll.lookup<ffi.NativeFunction<copy_paste_func>>('paste').asFunction();
+    dll.lookup<ffi.NativeFunction<CopyPasteFunc>>('paste').asFunction();
